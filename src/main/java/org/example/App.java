365423ba09fc430f79fca7b27a9e48f5ec3ef52f
Shikhin Misra <shikhinmisra@gmail.com>
2023-02-22 20:34:14 +0100

    Starting of project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,11 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+
+Adding random commits from feature_branch
+ */
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
